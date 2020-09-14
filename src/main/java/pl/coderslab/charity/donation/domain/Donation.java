2fd167e867fea7a5074e683ffa96162d624df641
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-09-14 21:40:19 +0200

    Created an entity: Donation, with relations to Category and Institution.

---
+++ src/main/java/pl/coderslab/charity/donation/domain/Donation.java
@@ -0,0 +1,53 @@
+package pl.coderslab.charity.donation.domain;
+
+import lombok.*;
+import pl.coderslab.charity.category.domain.Category;
+import pl.coderslab.charity.institution.domain.Institution;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Integer quantity;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = {CascadeType.ALL})
+    private List<Category> categories;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Institution institution;
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String zipCode;
+
+    @NotNull
+    private LocalDate pickUpDate;
+
+    @NotNull
+    private LocalTime pickUpTime;
+
+    @NotNull
+    private String pickUpComment;
+}
+
