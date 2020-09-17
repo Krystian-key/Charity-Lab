9829f896f80e236e0603b88c93279a4df6e789bb
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-09-17 14:34:37 +0200

    Finished InstitutionService with fully connected CRUD.

--- src/main/java/pl/coderslab/charity/institution/impl/InstitutionServiceImpl.java
+++ src/main/java/pl/coderslab/charity/institution/impl/InstitutionServiceImpl.java
@@ -1,36 +1,53 @@
 package pl.coderslab.charity.institution.impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.institution.InstitutionRepository;
 import pl.coderslab.charity.institution.InstitutionService;
 import pl.coderslab.charity.institution.domain.Institution;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class InstitutionServiceImpl implements InstitutionService {
+
+    private final InstitutionRepository institutionRepository;
+    private final static String ERROR_MESSAGE = "Institution not found with id: ";
 
     @Override
     public List<Institution> findAll() {
-        return null;
+        return institutionRepository.findAll();
     }
 
     @Override
     public Institution findById(Long id) {
-        return null;
+        return institutionRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(ERROR_MESSAGE + id));
     }
 
     @Override
-    public Institution create(Institution institution) {
-        return null;
+    public Long create(Institution institution) {
+        Institution create = institutionRepository.save(institution);
+        return create.getId();
     }
 
     @Override
     public Institution update(Institution institution, Long id) {
-        return null;
+        Institution institutionById = institutionRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(ERROR_MESSAGE + id));
+        return institutionRepository.save(institutionById);
     }
 
     @Override
     public void delete(Long id) {
+        Institution institutionById = institutionRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(ERROR_MESSAGE + id));
+        institutionRepository.delete(institutionById);
 
     }
 }
