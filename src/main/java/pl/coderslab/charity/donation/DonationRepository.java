cbb37342f90374d55d899667a92cf3969e5c2d16
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-09-22 21:04:43 +0200

    Finished form.jsp task

--- src/main/java/pl/coderslab/charity/donation/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/donation/DonationRepository.java
@@ -2,7 +2,6 @@ package pl.coderslab.charity.donation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.donation.domain.Donation;
 
 
